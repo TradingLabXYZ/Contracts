cbe277caa5ca1021f355b2bf8785fb81d2985619
dolphinxyz <robimalco@gmail.com>
2022-01-22 10:52:19 +0100

    #1 use solc and abigen

--- manage_deploy.sh
+++ manage_deploy.sh
@@ -1,13 +1,31 @@
 #/bin/sh
 truffle migrate --network dev --reset
-truffle run abigen SubscriptionModel
-node scripts/extract_contract_info.js
 
-# Contract JSON to frontend
-cp build/contracts/SubscriptionModel.json $HOME/Code/TradingLab/WebFront/src/functions
+echo "Processing PlansStorage"
+solcjs --abi contracts/PlansStorage.sol > PlansStorage.abi
+solcjs --bin contracts/PlansStorage.sol > PlansStorage.bin
+mv contracts_PlansStorage_sol_PlansStorage.abi ./abi/PlansStorage.abi
+mv contracts_PlansStorage_sol_PlansStorage.bin ./bin/PlansStorage.bin
+rm PlansStorage.abi PlansStorage.bin
+abigen --bin=./bin/PlansStorage.bin --abi=./abi/PlansStorage.abi --pkg=plansStorage --out=./golang/ContractPlansStorage.go
+cp ./golang/ContractPlansStorage.go /home/dolphin/Code/TradingLab/WebBack
 
-# Abigen to backend
-cp $HOME/Code/TradingLab/Contracts/abigenBindings/abi/SubscriptionModel.abi $HOME/Code/TradingLab/WebBack/contracts
+echo "Processing SubscriptionStorage"
+solcjs --abi contracts/SubscriptionsStorage.sol > SubscriptionsStorage.abi
+solcjs --bin contracts/SubscriptionsStorage.sol > SubscriptionsStorage.bin
+mv contracts_SubscriptionsStorage_sol_SubscriptionsStorage.abi ./abi/SubscriptionsStorage.abi
+mv contracts_SubscriptionsStorage_sol_SubscriptionsStorage.bin ./bin/SubscriptionsStorage.bin
+rm SubscriptionsStorage.abi SubscriptionsStorage.bin
+abigen --bin=./bin/SubscriptionsStorage.bin --abi=./abi/SubscriptionsStorage.abi --pkg=subscriptionsStorage --out=./golang/ContractSubscriptionsStorage.go
+cp ./golang/ContractSubscriptionsStorage.go /home/dolphin/Code/TradingLab/WebBack
 
-# Events to backend
-cp $HOME/Code/TradingLab/Contracts/scripts/subscription_info.json $HOME/Code/TradingLab/WebBack/contracts
+echo "Processing SubscriptionModel"
+solcjs --abi contracts/SubscriptionModel.sol > SubscriptionModel.abi
+solcjs --bin contracts/SubscriptionModel.sol > SubscriptionModel.bin
+mv contracts_SubscriptionModel_sol_SubscriptionModel.abi ./abi/SubscriptionModel.abi
+mv contracts_SubscriptionModel_sol_SubscriptionModel.bin ./bin/SubscriptionModel.bin
+rm SubscriptionModel.abi SubscriptionModel.bin
+abigen --bin=./bin/SubscriptionsStorage.bin --abi=./abi/SubscriptionsStorage.abi --pkg=subscriptionModel --out=./golang/ContractSubscriptionModel.go
+cp ./golang/ContractSubscriptionModel.go /home/dolphin/Code/TradingLab/WebBack
+
+rm -rf contracts_*
