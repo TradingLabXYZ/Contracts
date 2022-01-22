14246dc5ee9aae6b5277eb8e351f67dce9bda66c
dolphinxyz <robimalco@gmail.com>
2022-01-22 19:02:01 +0100

    #1 improved script

--- manage_deploy.sh
+++ manage_deploy.sh
@@ -9,7 +9,7 @@ mv contracts_PlansStorage_sol_PlansStorage.bin ./bin/PlansStorage.bin
 rm PlansStorage.abi PlansStorage.bin
 abigen --bin=./bin/PlansStorage.bin --abi=./abi/PlansStorage.abi --pkg=plansStorage --out=./golang/ContractPlansStorage.go
 sed -i '4s/.*/package main/' ./golang/ContractPlansStorage.go
-cp ./golang/ContractPlansStorage.go /home/dolphin/Code/TradingLab/WebBack
+cp ./golang/ContractPlansStorage.go $HOME/Code/TradingLab/WebBack
 
 echo "Processing SubscriptionStorage"
 solcjs --abi contracts/SubscriptionsStorage.sol > SubscriptionsStorage.abi
@@ -19,7 +19,7 @@ mv contracts_SubscriptionsStorage_sol_SubscriptionsStorage.bin ./bin/Subscriptio
 rm SubscriptionsStorage.abi SubscriptionsStorage.bin
 abigen --bin=./bin/SubscriptionsStorage.bin --abi=./abi/SubscriptionsStorage.abi --pkg=subscriptionsStorage --out=./golang/ContractSubscriptionsStorage.go
 sed -i '4s/.*/package main/' ./golang/ContractSubscriptionsStorage.go
-cp ./golang/ContractSubscriptionsStorage.go /home/dolphin/Code/TradingLab/WebBack
+cp ./golang/ContractSubscriptionsStorage.go $HOME/Code/TradingLab/WebBack
 
 echo "Processing SubscriptionModel"
 solcjs --abi contracts/SubscriptionModel.sol > SubscriptionModel.abi
@@ -29,10 +29,14 @@ mv contracts_SubscriptionModel_sol_SubscriptionModel.bin ./bin/SubscriptionModel
 rm SubscriptionModel.abi SubscriptionModel.bin
 abigen --bin=./bin/SubscriptionModel.bin --abi=./abi/SubscriptionModel.abi --pkg=subscriptionModel --out=./golang/ContractSubscriptionModel.go
 sed -i '4s/.*/package main/' ./golang/ContractSubscriptionModel.go
-cp ./golang/ContractSubscriptionModel.go /home/dolphin/Code/TradingLab/WebBack
+cp ./golang/ContractSubscriptionModel.go $HOME/Code/TradingLab/WebBack
 
 echo "Processing frontend"
 cp build/contracts/SubscriptionModel.json $HOME/Code/TradingLab/WebFront/src/functions
+
+CONTRACT_ADDRESS=$(jq '.networks."1281".address' $HOME/Code/TradingLab/Contracts/build/contracts/SubscriptionModel.json)
+CONTRACT_ADDRESS="${CONTRACT_ADDRESS//\"/}"
+sed -i "s/^export CONTRACT.*$/export CONTRACT=$CONTRACT_ADDRESS/g" $HOME/Code/TradingLab/WebBack/set_env_variables.sh
 
 rm -rf contracts_*
 
